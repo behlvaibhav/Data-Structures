0834171458856b9ce7dba9414dcb1a6e20cd0c18
Vaibhav Behl <behlvaibhav28@gmail.com>
2019-07-11 13:49:04 +0530

    factorial using recusrion

---
+++ recursion and backtracking/factorial.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+	if(n==0){
+		return 1;
+	}
+	int ans = n*fact(n-1);
+	return ans;
+}
+
+
+int main(){
+	int n;
+	cout<<"Enter the number whose factorial is required : ";
+	cin>>n;
+	cout<<fact(n);
+	return 0;
+}
