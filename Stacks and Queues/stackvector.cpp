e7a979a107c8df451e213b25ec66d22c21d2371b
Vaibhav Behl <behlvaibhav28@gmail.com>
2019-07-29 14:32:06 +0530

    vector as stack

---
+++ Stacks and Queues/stackvector.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Stack{
+	private:
+		vector<int>v;
+	
+	public:
+		void push(int data){
+			v.push_back(data);
+		}
+		
+		bool empty(){
+			return v.size()==0;// returns true if size of the vector is 0.
+		}
+		void pop(){
+			if(!empty()){
+				v.pop_back();
+			}
+		}
+		
+		int top(){
+			return v[v.size()-1];
+		}
+};
+
+int main(){
+	Stack s;
+	
+	for(int i=1;i<=5;i++){
+		s.push(i*i);
+	}
+	
+	//printing each element while popping them
+	while(!s.empty()){
+		cout<<s.top()<<endl;
+		s.pop();
+	}
+}
